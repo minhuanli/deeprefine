c586ccf04158f26bf69b0781d31245de37c69a32
minhuanli <minhuanli@g.harvard.edu>
2023-02-08 15:49:07 -0500

    update API

---
+++ deeprefine/__init__.py
@@ -0,0 +1,6 @@
+# Submodules
+from deeprefine import utils
+from deeprefine import energy
+
+# Top level API
+from deeprefine.energy.openmm import setup_protein
---
+++ deeprefine/utils/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "prep_pdb",
+    "align_md",
+    "remove_hydrogens",
+    "fix_missingresidues",
+]
+
+from deeprefine.utils.io import (
+    prep_pdb, 
+    align_md,
+    remove_hydrogens,
+    fix_missingresidues,
+)
