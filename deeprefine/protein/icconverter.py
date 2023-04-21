0922c571b89d447aa64d2b39f8ed22df9f131ff8
minhuanli <minhuanli@g.harvard.edu>
2023-04-21 15:47:28 -0400

    black formatting

--- deeprefine/protein/icconverter.py
+++ deeprefine/protein/icconverter.py
@@ -1,6 +1,6 @@
-'''
+"""
 Convert between cartesian coordinates and Internal Coordinates of protein
-'''
+"""
 import torch
 import numpy as np
 
@@ -9,13 +9,13 @@ from deeprefine.protein.zmatrix import get_indices
 
 
 def ics2xyz_local(ics, Z_indices, index2zorder, xyz):
-    """ For systems described in both internal coordinates and cartesian coordinates: convert ic to Cartesian
+    """For systems described in both internal coordinates and cartesian coordinates: convert ic to Cartesian
 
     Parameters
     ----------
     ics : Tenosr, [n_batches, n_atoms_to_place, 3]
         IC matrix by atom to place [[[bond1, angle1, torsion1], [bond2, angle2, torsion2], ...] ...]
-    
+
     Z_indices : array, [n_atoms_to_place, 4]
 
     index2order: array, [n_atoms,]
@@ -29,12 +29,12 @@ def ics2xyz_local(ics, Z_indices, index2zorder, xyz):
     p2s = xyz[:, index2zorder[Z_indices[:, 2]], :]
     p3s = xyz[:, index2zorder[Z_indices[:, 3]], :]
 
-    newpos = ic2xyz_torch(
-        p1s, p2s, p3s, ics[..., 0:1], ics[..., 1:2], ics[..., 2:3])
+    newpos = ic2xyz_torch(p1s, p2s, p3s, ics[..., 0:1], ics[..., 1:2], ics[..., 2:3])
     return newpos
 
+
 class ICConverter(object):
-    ''' Conversion between Cartesian coordinates and Cartesian / internal coordinates '''
+    """Conversion between Cartesian coordinates and Cartesian / internal coordinates"""
 
     def __init__(self, topology, vec_angles=True):
         """
@@ -42,7 +42,7 @@ class ICConverter(object):
         ----------
         topology: mdtraj.topology
             Topology of the protein
-        
+
         vec_angles: binary, default True
             Whether to use vector representation of all angles
         """
@@ -51,44 +51,56 @@ class ICConverter(object):
         self.vec_angles = vec_angles
 
         self.cart_signal_indices = np.concatenate(
-            [[i*3, i*3+1, i*3+2] for i in self.cart_atom_indices])
+            [[i * 3, i * 3 + 1, i * 3 + 2] for i in self.cart_atom_indices]
+        )
         self.dim_cart_signal = self.cart_signal_indices.size
 
         self.batchwise_Z_indices = decompose_Z_indices(
-            self.cart_atom_indices, self.Z_indices_no_order)
-        self.Z_indices = np.vstack(self.batchwise_Z_indices) 
-        
+            self.cart_atom_indices, self.Z_indices_no_order
+        )
+        self.Z_indices = np.vstack(self.batchwise_Z_indices)
+
         if self.vec_angles:
-            self.dim = 3*self.cart_atom_indices.size + 5*self.Z_indices.shape[0]
+            self.dim = 3 * self.cart_atom_indices.size + 5 * self.Z_indices.shape[0]
             n_internal = self.dim - self.dim_cart_signal
             self.bond_idxs = self.dim_cart_signal + np.arange(n_internal // 5) * 5 + 0
-            self.sinangle_idxs = self.dim_cart_signal + np.arange(n_internal // 5) * 5 + 1
-            self.cosangle_idxs = self.dim_cart_signal + np.arange(n_internal // 5) * 5 + 2
-            self.sintorsion_idxs = self.dim_cart_signal + np.arange(n_internal // 5) * 5 + 3
-            self.costorsion_idxs = self.dim_cart_signal + np.arange(n_internal // 5) * 5 + 4
+            self.sinangle_idxs = (
+                self.dim_cart_signal + np.arange(n_internal // 5) * 5 + 1
+            )
+            self.cosangle_idxs = (
+                self.dim_cart_signal + np.arange(n_internal // 5) * 5 + 2
+            )
+            self.sintorsion_idxs = (
+                self.dim_cart_signal + np.arange(n_internal // 5) * 5 + 3
+            )
+            self.costorsion_idxs = (
+                self.dim_cart_signal + np.arange(n_internal // 5) * 5 + 4
+            )
         else:
-            self.dim = 3*(self.cart_atom_indices.size + self.Z_indices.shape[0])
+            self.dim = 3 * (self.cart_atom_indices.size + self.Z_indices.shape[0])
             n_internal = self.dim - self.dim_cart_signal
             self.bond_idxs = self.dim_cart_signal + np.arange(n_internal // 3) * 3 + 0
             self.angle_idxs = self.dim_cart_signal + np.arange(n_internal // 3) * 3 + 1
-            self.torsion_idxs = self.dim_cart_signal + np.arange(n_internal // 3) * 3 + 2
+            self.torsion_idxs = (
+                self.dim_cart_signal + np.arange(n_internal // 3) * 3 + 2
+            )
 
         self.atom_order = np.concatenate([self.cart_atom_indices, self.Z_indices[:, 0]])
         self.index2order = np.argsort(self.atom_order)
 
     @classmethod
     def from_dict(cls, d):
-        CT = cls(d['topology'], vec_angles=d['vec_angles'])
+        CT = cls(d["topology"], vec_angles=d["vec_angles"])
         return CT
 
     def to_dict(self):
         d = {}
-        d['topology'] = self.topology
-        d['vec_angles'] = self.vec_angles
+        d["topology"] = self.topology
+        d["vec_angles"] = self.vec_angles
         return d
 
     def xyz2ic(self, x):
-        """ Convert cartesian coordinates to internal coordinates
+        """Convert cartesian coordinates to internal coordinates
 
         Parameters
         ----------
@@ -98,22 +110,22 @@ class ICConverter(object):
 
         Returns
         -------
-        z : Tensor, [n_batch, n_features] 
+        z : Tensor, [n_batch, n_features]
             flattened internal coordinates of full protein atoms
             The order of atoms is the same as the grouped Z_indices
         """
         # split off Cartesian coordinates of root atoms
-        x_cart = x[:,self.cart_signal_indices]
+        x_cart = x[:, self.cart_signal_indices]
 
         # Compute internal coordinates for remaining atoms
         z_ics = xyz2ic_torch(x, self.Z_indices, vec_angles=self.vec_angles)
-        
+
         # concatenate the output
         z = torch.concat([x_cart, z_ics], dim=1)
         return z
 
     def ic2xyz(self, z):
-        """ Convert internal coordinates to cartesian coordinates 
+        """Convert internal coordinates to cartesian coordinates
 
         Parameters
         ----------
@@ -129,14 +141,14 @@ class ICConverter(object):
         """
 
         # Get the cartesian of root atoms
-        x_cart = z[:, :self.dim_cart_signal]
+        x_cart = z[:, : self.dim_cart_signal]
         # split by atom
         batchsize = z.shape[0]
         xyz = x_cart.view(batchsize, self.cart_atom_indices.size, 3)
-        
+
         if self.vec_angles:
             bonds = z[:, self.bond_idxs]
-            
+
             cosangle = z[:, self.cosangle_idxs]
             sinangle = z[:, self.sinangle_idxs]
             angles = torch.atan2(sinangle, cosangle)
@@ -145,7 +157,7 @@ class ICConverter(object):
             sintorsion = z[:, self.sintorsion_idxs]
             torsions = torch.atan2(sintorsion, costorsion)
         else:
-            bonds =  z[:, self.bond_idxs]
+            bonds = z[:, self.bond_idxs]
             angles = z[:, self.angle_idxs]
             torsions = z[:, self.torsion_idxs]
 
@@ -153,7 +165,7 @@ class ICConverter(object):
 
         istart = 0
         for Z_indices in self.batchwise_Z_indices:
-            ics = z_ics[:, istart:(istart+Z_indices.shape[0]), :]
+            ics = z_ics[:, istart : (istart + Z_indices.shape[0]), :]
             newpos = ics2xyz_local(ics, Z_indices, self.index2order, xyz)
             xyz = torch.concat([xyz, newpos], dim=1)
             istart += Z_indices.shape[0]
