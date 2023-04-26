c47f53552674caea4bceb1d6c2bf94b67f866c14
minhuanli <minhuanli@g.harvard.edu>
2023-04-25 22:44:19 -0400

    finishe realnvp block

---
+++ deeprefine/nn/basics.py
@@ -0,0 +1,128 @@
+import numbers
+import numpy as np
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DenseLayer(nn.Module):
+    """Just your regular densely-connected NN layer
+
+    Parameters
+    ----------
+    in_features : int
+        Size of each input sample
+    out_features : int
+        Size of each output sample
+    activation : str, default None
+        Nonlinear activation type
+    dropout: float, 0 - 1, default None
+        Probability of an element to be zeroed
+    """
+
+    def __init__(self, in_features, out_features, activation=None, dropout=None):
+        super(DenseLayer, self).__init__()
+        self.linear = nn.Linear(in_features, out_features, bias=True)
+        self.activation = self._get_activation(activation)
+        self.dropout = self._get_dropout(dropout)
+
+    def _get_activation(self, activation):
+        if activation is None:
+            return nn.Identity()
+        elif isinstance(activation, str):
+            try:
+                return getattr(F, activation)
+            except:
+                raise ValueError(
+                    "Unsupported activation function: {}".format(activation)
+                )
+        else:
+            raise ValueError("Need a valid activation type in str")
+
+    def _get_dropout(self, dropout):
+        if dropout is None:
+            return nn.Identity()
+        elif isinstance(dropout, float):
+            return nn.Dropout(dropout)
+        else:
+            raise ValueError("Need a valid dropout rate in float, between 0 and 1.")
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = self.activation(x)
+        x = self.dropout(x)
+        return x
+
+
+class DenseNet(nn.Module):
+    """Generic dense trainable nonlinear transform
+
+    Returns the layers of a dense feedforward network with nlayers-1 hidden layers with nhidden neurons
+    and the specified activation functions. The last layer is linear in order to access the full real
+    number range and has output_size output neurons.
+
+    Parameters
+    ----------
+    input_size :  int
+        number of input neurons
+    output_size : int
+        number of output neurons
+    nlayers : int
+        number of layers, including the linear output layer. nlayers=3 means two hidden layers with
+        nonlinear activation and one linear output layer.
+    nhidden : int
+        number of neurons in each hidden layer, either a number or an array of length nlayers-1
+        to specify the width of each hidden layer
+    activation : str
+        nonlinear activation function in hidden layers
+    init_outputs : None or float or array
+        None means default initialization for the output layer, otherwise it is currently initialized with 0
+    **args : kwargs
+        Additional keyword arguments passed to the layer
+    """
+
+    def __init__(
+        self,
+        input_size,
+        output_size,
+        nlayers=3,
+        nhidden=100,
+        activation="relu",
+        dropout=None,
+        init_outputs=None,
+        **args
+    ):
+        super().__init__()
+        if isinstance(nhidden, numbers.Integral):
+            nhidden = nhidden * np.ones(nlayers - 1, dtype=int)
+        else:
+            nhidden = np.array(nhidden)
+            if nhidden.size != nlayers - 1:
+                raise ValueError(
+                    "Illegal size of nhidden. Expecting 1d array with nlayers-1 elements"
+                )
+        assert nlayers > 1, "nlayers should at least be 2!"
+
+        layers = []
+        layers.append(
+            DenseLayer(
+                input_size, nhidden[0], activation=activation, dropout=dropout, **args
+            )
+        )
+        for i in range(nlayers - 2):
+            layers.append(
+                DenseLayer(
+                    nhidden[i], nhidden[i+1], activation=activation, dropout=dropout, **args
+                )
+            )
+        if init_outputs is None:
+            final_layer = nn.Linear(nhidden[-1], output_size, **args)
+        else:
+            final_layer = nn.Linear(nhidden[-1], output_size, **args)
+            nn.init.zeros_(final_layer.weight.data)
+            nn.init.constant_(final_layer.bias.data, init_outputs)
+        layers.append(final_layer)
+        self._layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self._layers(x)
--- deeprefine/nn/flow/invertible_layers.py
+++ deeprefine/nn/flow/invertible_layers.py
@@ -3,6 +3,7 @@ import torch
 
 import torch.nn as nn
 from deeprefine.nn.flow.basics import Flow, InverseFlow
+from deeprefine.nn.basics import DenseNet
 
 
 def split_merge_indices(ndim, nchannels=2, channels=None):
@@ -85,5 +86,136 @@ class MergeChannels(InverseFlow):
     channels : None or list or array, default None
         Index of channel for each feature
     """
+
     def __init__(self, ndim, nchannels=2, split_dim=-1, channels=None):
         super().__init__(SplitChannels(ndim, nchannels, split_dim, channels))
+
+
+class RealNVP(Flow):
+    def __init__(
+        self,
+        dim_L,
+        dim_R,
+        n_layers=2,
+        n_hidden=100,
+        activation="relu",
+        n_layers_scale=None,
+        n_hidden_scale=None,
+        activation_scale="tanh",
+        init_output_scale=0,
+        **layer_args
+    ):
+        """Two sequential NVP transformations and their inverse transformatinos.
+
+        Parameters
+        ----------
+        dim_L : int
+            Number of features on the left channel
+        dim_R : int
+            Number of features on the right channel
+        n_layers : int
+            Number of hidden layers in the nonlinear transformations
+        n_hidden : int
+            Number of hidden units in each nonlinear layer
+        activation : str
+            Hidden-neuron activation functions used in the nonlinear layers
+        n_layers_scale : int
+            Number of hidden layers in the nonlinear transformations for the scaling network
+            if None, will use n_layers
+        n_hidden_scale : int
+            Number of hidden units in each nonlinear layer for the scaling network.
+            if None, will use n_hidden
+        activation_scale : str
+            Hidden-neuron activation functions used in scaling networks. If None, nl_activation will be used.
+            if None, will use activation
+        init_output_scale : float
+            Initialize weights and bias of scaling networks to set the initial output value
+        """
+        super().__init__()
+        if activation_scale is None:
+            activation_scale = activation
+        if n_layers_scale is None:
+            n_layers_scale = n_layers
+        if n_hidden_scale is None:
+            n_hidden_scale = n_hidden
+
+        self.S1 = DenseNet(
+            dim_L,
+            dim_R,
+            nlayers=n_layers_scale,
+            nhidden=n_hidden_scale,
+            activation=activation_scale,
+            init_outputs=init_output_scale,
+            **layer_args
+        )
+        self.T1 = DenseNet(
+            dim_L,
+            dim_R,
+            nlayers=n_layers,
+            nhidden=n_hidden,
+            activation=activation,
+            **layer_args
+        )
+        self.S2 = DenseNet(
+            dim_R,
+            dim_L,
+            nlayers=n_layers_scale,
+            nhidden=n_hidden_scale,
+            activation=activation_scale,
+            init_outputs=init_output_scale,
+            **layer_args
+        )
+        self.T2 = DenseNet(
+            dim_R,
+            dim_L,
+            nlayers=n_layers,
+            nhidden=n_hidden,
+            activation=activation,
+            **layer_args
+        )
+
+    def _forward(self, x):
+
+        def lambda_sum(x):
+            return torch.sum(x[0], dim=-1, keepdim=True) + torch.sum(x[1], dim=-1, keepdim=True)
+
+        x1 = x[0] # dim_L
+        x2 = x[1] # dim_R
+
+        y1 = x1
+        s1x1 = self.S1(x1)
+        t1x1 = self.T1(x1)
+        y2 = x2 * torch.exp(s1x1) + t1x1 # dim_R
+
+        self.output_z2 = y2
+        s2y2 = self.S2(y2)
+        t2y2 = self.T2(y2)
+        self.output_z1 = y1 * torch.exp(s2y2) + t2y2  # dim_L
+
+        # log det(dz/dx)
+        self.log_det_Jxz = lambda_sum([s1x1, s2y2])
+
+        return [self.output_z1, self.output_z2], self.log_det_Jxz
+
+    def _inverse(self, z):
+
+        def lambda_negsum(x):
+            return torch.sum(-x[0], dim=-1, keepdim=True) + torch.sum(-x[1], dim=-1, keepdim=True)
+
+        z1 = z[0] # dim_L
+        z2 = z[1] # dim_R
+
+        y2 = z2
+        s2z2 = self.S2(z2)
+        t2z2 = self.T2(z2)
+        y1 = (z1 - t2z2) * torch.exp(-s2z2) # dim_L
+
+        self.output_x1 = y1
+        s1y1 = self.S1(y1)
+        t1y1 = self.T1(y1)
+        self.output_x2 = (y2 - t1y1) * torch.exp(-s1y1) # dim_R
+
+        # log det(dx/dz)
+        self.log_det_Jzx = lambda_negsum([s2z2, s1y1])
+
+        return [self.output_x1, self.output_x2], self.log_det_Jzx
